2869fbf72abdc6e970cbcff75a26af6eeb26ad8c
Othniel Tiendrebeogo <othnieltiendrebeogo@Othniels-MacBook-Pro.local>
2021-10-24 22:38:33 -0400

    myFile2.h added

--- Ex2/myFile2.cpp
+++ Ex2/myFile2.cpp
@@ -1,16 +1,17 @@
 /*Ex2 devoir4 CSI2772A*/
 
-
 #include <iostream> 
 #include "myFile2.h"
+
 using namespace std;
 
 
 int main() {
     SetInt a; 	// object creation
     int* tab;
-
     
+
+
 
     while (a.nbElem() < 5)
     {
@@ -32,3 +33,163 @@ int main() {
 
     return 0;
 }
+
+// contructor 
+SetInt :: SetInt(const SetInt& n ) {
+    elem =  n.elem; 
+    size = n.size ; 
+}
+
+
+SetInt :: SetInt(int tab [] , int taille ){
+
+   size =taille ;
+
+   elem =new int [size];
+
+   for (int i = 0 ; i< size ; i++){
+
+       elem[i] = tab[i];
+   }
+
+}
+
+
+// constructor 
+
+
+// destructor 
+
+SetInt :: ~SetInt(){
+
+
+};
+
+// destructor 
+
+
+
+// ************functions **************//
+
+ bool SetInt ::contains(int n){
+
+    for (int i = 0; i < size; i++){
+
+        if ( elem[i] == n ){
+
+            return  true ; 
+        }
+
+    }
+  
+   return false ; 
+ 
+}
+
+
+
+bool SetInt :: containsIn(int n, int &x ){
+
+      int * pointer = &x;
+     int  pos = *pointer;
+
+      if(pos > size){
+
+          return false; 
+      }
+
+      if (elem[pos]== n ){
+
+          return true;
+           
+
+
+      }
+      return false;
+
+}
+
+
+
+
+void SetInt :: add(int n){
+
+ if(contains (n) == false) {
+
+int * pointer = new int [size +1]; // tableau dynamic
+
+for ( int i = 0 ; i < size ; i++ ){
+
+pointer[i] = elem[i];
+
+}
+
+pointer[size++] = n ;
+
+elem = pointer; //
+
+pointer = NULL;
+
+delete [] pointer;
+
+ }
+
+}
+
+void SetInt ::  remove(int n ) {
+
+
+    int i ;
+
+    for( i = 0 ; i < size ; i++ ){
+
+
+        if (elem[i] == n){
+            break;
+        }  
+    }
+
+    if(i < size){
+
+          size =   size -1 ;
+
+            for ( int j = i ; j < size ; j++ ){
+
+                elem[j] = elem[j+1];
+            }
+           
+        }
+}
+
+
+int*  SetInt ::  tabElem() {
+
+     if (size == 0 ) {
+
+         return NULL ; 
+     }
+
+      
+      int * tab = new int[size];
+
+     for ( int i = 0 ; i< size ; i++ ){
+
+        tab[i] = elem[i] ;
+     }
+
+     return tab ; 
+ }
+
+
+ int SetInt :: nbElem() {
+
+
+     return size ;
+ }
+
+
+
+
+
+
+
